8edd95e56eafbfcc245af3a2d03fe0babf6ecb3a
Marco de Sousa <desousa.marco@gmail.com>
2019-08-30 14:18:09 +0200

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -6,24 +6,22 @@ import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment } from '../shared/comment';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility } from '../animations/app.animations';
+import { flyInOut, expand } from '../animations/app.animations';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
+  // tslint:disable-next-line:use-host-property-decorator
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    flyInOut(),
+    visibility(),
+    expand()
   ]
 })
 
@@ -46,7 +44,7 @@ export class DishdetailComponent implements OnInit {
   };
 
   visibility = 'shown';
-
+  errMsg: string;
 
   @ViewChild('cform') commentFormDirective;
   @ViewChild('ratingslider') slider;
@@ -80,7 +78,9 @@ export class DishdetailComponent implements OnInit {
 
   ngOnInit() {
     this.dishService.getDishIds()
-      .subscribe((dishIds) => this.dishIds = dishIds);
+      .subscribe((dishIds) => this.dishIds = dishIds,
+        errmsg => this.errMsg = <any>errmsg
+        );
 
     this.route.params
       .pipe(switchMap(( params: Params ) => {
